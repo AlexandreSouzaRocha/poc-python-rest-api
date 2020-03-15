57b2b582c27126853d490be8474ee9415aae7a65
Alexandre Rocha <alexandresouzarocha943@gmail.com>
2020-03-15 17:32:00 -0300

    app.py: fixing code and routes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Category import CategoryResource
+from resources.Comments import CommentResource
+from resources.AsyncRequest import AsynchronousResource
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 api.add_resource(Hello, '/hello')
+api.add_resource(CategoryResource, '/category')
+api.add_resource(CommentResource, '/comment')
+api.add_resource(AsynchronousResource, '/users')
